f4c298d57c4c69c7347fcfa39f3d6f721b2756e3
John Browning <john_jsb@hotmail.com>
2017-03-20 12:32:00 -0400

    deleted files

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/talos/talos-ros-robot/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/talos/kalibr_workspace/devel;/opt/ros/kinetic".split(';') if "/home/talos/kalibr_workspace/devel;/opt/ros/kinetic" != "" else []
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/talos/talos-ros-robot/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
